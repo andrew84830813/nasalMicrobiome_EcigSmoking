e063f143915edcb3ef195acb030b5bbd89df5cd0
Andrew Hinton <68709832+andrew84830813@users.noreply.github.com>
2021-07-16 05:35:20 -0400

    Fixed missing file read

--- Rscript/Data Preprocess/data_preProcessNLF.R
+++ Rscript/Data Preprocess/data_preProcessNLF.R
@@ -17,6 +17,7 @@ library(stringr)
 ############################################################-
 ## Read Data ####
 ############################################################-
+metadata = read_csv("Data/2019_09_13 ELFmetadata.csv")
 NLF_protein = read_csv("Data/2020_06_11 NLF Data.csv")
 nlf_df = left_join(metadata,NLF_protein)
 ############################################################-
@@ -69,37 +70,6 @@ df = df %>%
   dplyr::rename(Status = Sex)
 ## Output  -  << NLF by Subject Group >>
 write.csv(df,"Output/NLF_bySex.csv",row.names = T)
-## NLF with Nasal Microbiome by Sex ####
-df = data.frame(X1 = rownames(df),df)
-nasalMicrobiome = read_csv(file = "Output/bySex_nasalMbiome.csv")
 ############################################################-
-
-
-
-
-############################################################-
-## NLF with Nasal Microbiome by Sex ####
-############################################################-
-df = nlf_df %>%
-  dplyr::select(Sex,one_of(colnames(NLF_protein)[-1])) #Sex
-df = data.frame(df)
-rownames(df) = nlf_df$SampleID
-#remove samples with missing data
-remove_row = which(is.na(df$Total.IgA..pg.mL.))
-df = df[-remove_row,]
-#Convert units so that everything is ug/ML
-df$DEFB4A.2..pg.mL. = df$DEFB4A.2..pg.mL.*1e-6
-df$DEFB1..pg.mL. = df$DEFB1..pg.mL.*1e-6
-df$Neutrophil.Elastase..ng.mL. = df$Neutrophil.Elastase..ng.mL.*0.001
-df$Total.IgA..pg.mL. = df$Total.IgA..pg.mL.*1e-6
-df$IL.8..pg.mL. = df$IL.8..pg.mL.*1e-6
-colnames(df) = str_replace(string = colnames(df),pattern = "pg",replacement = "ug")
-colnames(df) = str_replace(string = colnames(df),pattern = "ng",replacement = "ug")
-df = df %>%
-  dplyr::rename(Status = Sex)
-## Output  -  << NLF by Subject Group >>
-write.csv(df,"Output/NLF_bySex.csv",row.names = T)
-############################################################-
-
 
 
